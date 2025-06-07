6c6c6ef2587ef29deb5d46307d7362f018d85c2a
Chivy221 <danilius227@gmail.com>
2025-06-07 23:11:42 +0300

    Create Task.js

---
+++ models/Task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  assignedTo: String,
+  dueDate: Date,
+  status: { type: String, default: 'pending' }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
