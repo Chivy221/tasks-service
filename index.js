8f214ab9b230468368e17d508ac773e723789569
Chivy221 <danilius227@gmail.com>
2025-06-08 14:48:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-require('dotenv').config();
-
+const dotenv = require('dotenv');
+const morgan = require('morgan');
 const taskRoutes = require('./routes/tasks');
+const { logToFile } = require('./utils/logger');
+
+dotenv.config();
+const app = express();
+
 app.use(express.json());
+app.use(morgan('combined', { stream: { write: logToFile } }));
+
 app.use('/tasks', taskRoutes);
-app.get('/ping', (_, res) => res.send('pong'));
+
+app.get('/ping', (, res) => res.send('pong'));
+app.get('/health', (, res) => res.json({ status: 'ok' }));
 
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
 }).then(() => {
-  console.log('Tasks DB connected');
-  app.listen(3000, () => console.log('Tasks service on port 3000'));
-}).catch(err => console.error(err));
+console.log('Connected to MongoDB');
+app.listen(3000, () => console.log('Tasks Service running on port 3000'));
+}).catch(console.error);
