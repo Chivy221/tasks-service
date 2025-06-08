a0c48762a85c5fdb5c652141fb42ed37df01b2a0
Chivy221 <danilius227@gmail.com>
2025-06-08 22:05:54 +0300

    Update crypto.js

--- utils/crypto.js
+++ utils/crypto.js
@@ -2,13 +2,17 @@ const crypto = require('crypto');
 const key = process.env.CRYPTO_PASSWORD;
 
 function encrypt(text) {
-const cipher = crypto.createCipher('aes-256-ctr', key);
-return cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+  const cipher = crypto.createCipher('aes-256-ctr', key);
+  return cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
 }
 
 function decrypt(text) {
-const decipher = crypto.createDecipher('aes-256-ctr', key);
-return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+  try {
+    const decipher = crypto.createDecipher('aes-256-ctr', key);
+    return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+  } catch (e) {
+    return text;
+  }
 }
 
 module.exports = { encrypt, decrypt };
