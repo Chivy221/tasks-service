b3402128633095351e6d2dd4c86c3cca59121258
Chivy221 <danilius227@gmail.com>
2025-06-08 14:49:48 +0300

    Update task.js

--- routes/task.js
+++ routes/task.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
+const encrypt = require('../middlewares/encrypt');
+const cache = require('../middlewares/cache');
+const { sendTaskCreated } = require('../utils/rabbitmq');
 
-router.post('/', async (req, res) => {
-  try {
-    const task = new Task(req.body);
-    await task.save();
-    res.status(201).json(task);
-  } catch (e) {
-    res.status(400).json({ error: e.message });
-  }
+router.post('/', encrypt, async (req, res) => {
+const task = new Task(req.body);
+await task.save();
+await sendTaskCreated(task); // RabbitMQ
+res.status(201).json(task);
 });
 
-router.get('/', async (_, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
+router.get('/', cache, async (_, res) => {
+const tasks = await Task.find();
+res.json(tasks);
 });
 
 module.exports = router;
