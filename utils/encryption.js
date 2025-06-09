7a5b4699d91a0817b945525b8250d29a245d2817
Chivy221 <danilius227@gmail.com>
2025-06-09 18:43:12 +0300

    Update encryption.js

--- utils/encryption.js
+++ utils/encryption.js
@@ -1,23 +1,32 @@
 const crypto = require("crypto");
 
-const algorithm = "aes-256-ctr";
-const secretKey = process.env.ENCRYPTION_KEY || "default_key_that_should_be_secure_and_32_bytes";
-const ivLength = 16;
+// Ключ и IV должны быть 32 и 16 байт соответственно
+const ENCRYPTION_KEY = crypto
+  .createHash("sha256")
+  .update(String(process.env.ENCRYPTION_SECRET || "default_secret"))
+  .digest("base64")
+  .substr(0, 32);
 
-const encrypt = (text) => {
-  const iv = crypto.randomBytes(ivLength);
-  const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey, "utf-8"), iv);
-  const encrypted = Buffer.concat([cipher.update(text, "utf8"), cipher.final()]);
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
   return iv.toString("hex") + ":" + encrypted.toString("hex");
-};
+}
 
-const decrypt = (hash) => {
-  const [ivHex, encryptedHex] = hash.split(":");
-  const iv = Buffer.from(ivHex, "hex");
-  const encryptedText = Buffer.from(encryptedHex, "hex");
-  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey, "utf-8"), iv);
-  const decrypted = Buffer.concat([decipher.update(encryptedText), decipher.final()]);
-  return decrypted.toString("utf8");
-};
+function decrypt(text) {
+  const textParts = text.split(":");
+  const iv = Buffer.from(textParts.shift(), "hex");
+  const encryptedText = Buffer.from(textParts.join(":"), "hex");
+  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
+}
 
 module.exports = { encrypt, decrypt };
