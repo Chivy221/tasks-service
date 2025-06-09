1bd62db0eb4be5dbeec41cf0a07ed1aac7d75480
Chivy221 <danilius227@gmail.com>
2025-06-09 19:19:27 +0300

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,15 +1,18 @@
-const amqp = require('amqplib');
+const fs = require("fs");
+const path = require("path");
 
-async function sendLog(message) {
-try {
-const conn = await amqp.connect(process.env.RABBITMQ_URL);
-const ch = await conn.createChannel();
-await ch.assertQueue(process.env.LOG_QUEUE, { durable: false });
-ch.sendToQueue(process.env.LOG_QUEUE, Buffer.from(message));
-setTimeout(() => conn.close(), 500);
-} catch (e) {
-console.error('RabbitMQ log error:', e.message);
+const logDir = path.join(__dirname, "../logs");
+const logPath = path.join(logDir, "task.log");
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir); // создаёт папку logs, если её нет
 }
-}
+
+const sendLog = (message) => {
+  const logEntry = `[${new Date().toISOString()}] ${message}\n`;
+  fs.appendFile(logPath, logEntry, (err) => {
+    if (err) console.error("Failed to write log:", err);
+  });
+};
 
 module.exports = { sendLog };
