b9fdbc0b8bb5e389de9a9f3dcbbca977211265cf
Chivy221 <danilius227@gmail.com>
2025-06-09 17:59:19 +0300

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
+
 const taskSchema = new mongoose.Schema({
-title: String,
-description: String,
-assignedTo: String,
-status: { type: String, default: 'pending' },
-createdAt: { type: Date, default: Date.now }
+  id: { type: String, required: true },
+  title: String,
+  description: String,
+  assignedTo: String,
+  status: { type: String, default: 'pending' },
+  createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Task', taskSchema);
